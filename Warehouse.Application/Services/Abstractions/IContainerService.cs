0a922262654a6e4a2bf800a79227c29219ffd473
sergiy1111 <grigha71@gmail.com>
2022-10-28 19:17:06 +0300

    Add Container service

---
+++ Warehouse.Application/Services/Abstractions/IContainerService.cs
@@ -0,0 +1,13 @@
+﻿using Warehouse.Infrastructure.Entities;
+
+namespace Warehouse.Application.Services.Abstractions
+{
+    public interface IContainerService
+    {
+        Task<Container> AddAsync(Container container);
+        Task DeleteAsync(int id);
+        Task<Container[]> GetAllAsync();
+        Task<Container?> GetAsync(int id);
+        Task UpdateAsync(Container container);
+    }
+}
---
+++ Warehouse.Application/Services/ContainerService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Warehouse.Application.Services.Abstractions;
+using Warehouse.Infrastructure;
+using Warehouse.Infrastructure.Entities;
+
+namespace Warehouse.Application.Services
+{
+    internal class ContainerService : IContainerService
+    {
+        private readonly WarehouseContext context;
+
+        public ContainerService(WarehouseContext context)
+        {
+            this.context = context;
+        }
+
+        public Task<Container[]> GetAllAsync()
+            => context.Containers
+                .AsNoTracking()
+                .ToArrayAsync();
+
+        public Task<Container?> GetAsync(int id)
+            => context.Containers
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+        public async Task<Container> AddAsync(Container container)
+        {
+            await context.Containers.AddAsync(container);
+            await context.SaveChangesAsync();
+
+            return container;
+        }
+
+        public async Task UpdateAsync(Container container)
+        {
+            context.Containers.Update(container);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            context.Containers.Remove(new Container { Id = id });
+            await context.SaveChangesAsync();
+        }
+
+    }
+}
