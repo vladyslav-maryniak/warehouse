536d886c13049bf0ce118b4226da6ab2e75d171b
Vladyslav Maryniak <vladyslav.maryniak@gmail.com>
2022-10-31 20:52:17 +0200

    Add DTOs to Request controller endpoints

---
+++ Warehouse.API/AutoMapperProfiels/RequestProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Warehouse.API.DTOs.Requests;
+using Warehouse.Infrastructure.Entities;
+
+namespace Warehouse.API.AutoMapperProfiels
+{
+    public class RequestProfile : Profile
+    {
+        public RequestProfile()
+        {
+            CreateMap<AddRequestCommand, Request>();
+            CreateMap<UpdateRequestCommand, Request>();
+        }
+    }
+}
